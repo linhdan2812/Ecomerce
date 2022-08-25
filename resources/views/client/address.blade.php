1f0c8b2c045903b25b6a159d06967fc173cc674a
Exiler <59753774+Exilerr@users.noreply.github.com>
2022-08-25 23:15:15 +0700

    Code Api Location đơn giản

--- resources/views/client/address.blade.php
+++ resources/views/client/address.blade.php
@@ -98,4 +98,19 @@
             </div>
         </div>
     </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    $('#city').click(function(){
+        let url = "{{route('getCity')}}"
+        $.get(url).done(function( data ) {
+            let text = "";
+            data.forEach(element => {
+                text +=`
+                        <option value="${element.id}">${element.name}</option>
+                        `; 
+            });
+            document.getElementById("city").innerHTML = text;
+        });
+    })
+</script>
 @endsection
--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -68,6 +68,7 @@
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
     </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="
         {{ asset('client/account/js/jquery.js') }}" type="text/javascript"></script>
     <script src="
