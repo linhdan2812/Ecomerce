d848c53eaef0a62e518c52e18703c4ff0fbefb01
Exiler <59753774+Exilerr@users.noreply.github.com>
2022-09-16 07:25:45 +0700

    thay đổi trạng thái đơn hàng tại admin

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -19,9 +19,14 @@ class OrderController extends Controller
         return view('admin.orders.index',compact('orders'));
     }
 
-    public function detail($id){
-        $order = Order::find($id);
+    public function detail($order_number){
+        $order = Order::where('order_number',$order_number)->first();
         $order->load('shipping','coupon','user');
         return view('admin.orders.detail',compact('order'));
+    }
+    public function changestatus(Request $request){
+        $order = Order::where('id', $request->id)
+                ->update(['status' => $request->status == 1 ? 'Đã hủy' : 'Đang vận chuyển']);
+        return $order;
     }
 }
