1b5cb4842d388517483f1d6842b299f2256d2f69
Exilerr <59753774+Exilerr@users.noreply.github.com>
2022-12-10 14:37:22 +0700

    commite product in admin

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -32,15 +32,6 @@ class ProductController extends Controller
 
     public function saveAdd(ProductRequest $request)
     {
-        if ($request->hasFile('image')) {
-            $newFileName = uniqid() . '-' . $request->image[0]->getClientOriginalName();
-            $pathImage = $request->image[0]->storeAs('public/uploads/products', $newFileName);
-        }
-        $arr_tojson = array(
-            'size' => $request->size[0] ?? '',
-            'color' => $request->color[0] ?? '', 
-            'image' => $pathImage ?? '',
-        );
         if ($request->hasFile('photo')) {
             $newFileName = uniqid() . '-' . $request->photo->getClientOriginalName();
             $path = $request->photo->storeAs('public/uploads/products', $newFileName);
@@ -52,7 +43,9 @@ class ProductController extends Controller
             'price' => $request->price ?? '',
             'discount' => $request->discount ?? '',
             'photo' => str_replace('public/', '', $path) ?? '',
-            'style' => [json_encode($arr_tojson)] ?? '',
+            'size' => json_encode($request->size) ?? '',
+            'color' => json_encode($request->color) ?? '',
+            'status' => $request->status ?? 'active'
         ]);
         return redirect(route('admin.product.list'))->with('msg','Thêm mới thành công!');
     }
