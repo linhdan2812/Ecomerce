75c1961a19f29c1eeccf9538adf2642cf0813b76
Hieund <duyhieundh272000@gmail.com>
2022-12-15 22:22:28 +0700

    feat/send notification

--- app/Http/Controllers/VnpayController.php
+++ app/Http/Controllers/VnpayController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\NotificationEvent;
 use App\Mail\OrderMail;
 use App\Models\Order;
 use App\Models\Vnpay;
@@ -284,7 +285,9 @@ class VnpayController extends Controller
             }
             DB::commit();
             // $this->sendMail();
-            return redirect()->route('push-notification');
+            // return redirect()->route('push-notification');
+            event(new NotificationEvent('Bạn vừa có đơn hàng mới'));
+            return view('client.vnpay.return');
         } catch (Exception $e) {
             DB::rollBack();
             $returnData['RspCode'] = '99';
