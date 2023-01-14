477758438497e5dc07bb154e8a72fc3429f756b5
Ngọc Hoàng <hoangbn@d2t.vn>
2023-01-14 10:38:06 +0700

    commit

--- resources/views/admin/charts/firstChart.blade.php
+++ resources/views/admin/charts/firstChart.blade.php
@@ -63,116 +63,410 @@
     </figure>
 </div>
 <script>
-    $('#date').datepicker({  
-    format: 'mm-dd-yyyy'
-    });
-    let url = "{{route('firstChart')}}"
-    $.get(url,{
-        }).done(function( result ) {
-            var dataChart = result.data
-            dataChart.forEach(element => {
-                if( element.year == dataChart[0]['year']){
-                    console.log(1);
-                }
-            });
-            Highcharts.chart('container', {
-                chart: {
-                    type: 'line'
-                },
-                title: {
-                    text: 'Tổng hợp sản phẩm bán chạy trong 1 thời gian cố định'
-                },
-                xAxis: {
-                    categories: [1,2,3,4,5,6,7,8,9,10,11,12],
-                },
-                yAxis: {
-                    title: {
-                        text: ''
-                    }
-                },
-                plotOptions: {
-                    line: {
-                        dataLabels: {
-                            enabled: true
-                        },
-                        enableMouseTracking: false
-                    }
-                },
-                series: [{
-                    name: 'Reggane',
-                    data: [16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8, 38.4, 35.5, 29.2,
-                        22.0, 17.8]
-                }, {
-                    name: 'Tallinn',
-                    data: [-2.9, -3.6, -0.6, 4.8, 10.2, 14.5, 17.6, 16.5, 12.0, 6.5,
-                        2.0, -0.9]
-                }]
-            });
-        });
-    $('#search').click(function(){
-        let url = "{{route('firstChart')}}"
-        let from = $('#fromlistOrderASC').val()
-        let to = $('#toListUserASC').val();
-        $.get(url,{
-            from: from,
-            to: to
-        }).done(function( result ) {
-            var dataChart = {
-            ...result.chart
-            }
-            // var xAxis = Object.assign({}, result.xAxis);
-            // var yAxis = Object.assign({}, result.yAxis);
+    // $('#date').datepicker({  
+    // format: 'mm-dd-yyyy'
+    // });
+    // let url = "{{route('firstChart')}}"
+    // $.get(url,{
+    //     }).done(function( result ) {
+    //         var dataChart = result.data
+    //         dataChart.forEach(element => {
+    //             if( element.year == dataChart[0]['year']){
+    //                 console.log(1);
+    //             }
+    //         });
+    //         Highcharts.chart('container', {
+    //             chart: {
+    //                 type: 'line'
+    //             },
+    //             title: {
+    //                 text: 'Tổng hợp sản phẩm bán chạy trong 1 thời gian cố định'
+    //             },
+    //             xAxis: {
+    //                 categories: [1,2,3,4,5,6,7,8,9,10,11,12],
+    //             },
+    //             yAxis: {
+    //                 title: {
+    //                     text: ''
+    //                 }
+    //             },
+    //             plotOptions: {
+    //                 line: {
+    //                     dataLabels: {
+    //                         enabled: true
+    //                     },
+    //                     enableMouseTracking: false
+    //                 }
+    //             },
+    //             series: [{
+    //                 name: 'Reggane',
+    //                 data: [16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8, 38.4, 35.5, 29.2,
+    //                     22.0, 17.8]
+    //             }, {
+    //                 name: 'Tallinn',
+    //                 data: [-2.9, -3.6, -0.6, 4.8, 10.2, 14.5, 17.6, 16.5, 12.0, 6.5,
+    //                     2.0, -0.9]
+    //             }]
+    //         });
+    //     });
+    // $('#search').click(function(){
+    //     let url = "{{route('firstChart')}}"
+    //     let from = $('#fromlistOrderASC').val()
+    //     let to = $('#toListUserASC').val();
+    //     $.get(url,{
+    //         from: from,
+    //         to: to
+    //     }).done(function( result ) {
+    //         var dataChart = {
+    //         ...result.chart
+    //         }
+    //         Highcharts.chart('container', {
+    //             chart: {
+    //                 type: 'line'
+    //             },
+    //             title: {
+    //                 text: 'Monthly Average Temperature'
+    //             },
+    //             subtitle: {
+    //                 text: 'Source: ' +
+    //                     '<a href="https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature" ' +
+    //                     'target="_blank">Wikipedia.com</a>'
+    //             },
+    //             xAxis: {
+    //                 categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    //             },
+    //             yAxis: {
+    //                 title: {
+    //                     text: 'Temperature (°C)'
+    //                 }
+    //             },
+    //             plotOptions: {
+    //                 line: {
+    //                     dataLabels: {
+    //                         enabled: true
+    //                     },
+    //                     enableMouseTracking: false
+    //                 }
+    //             },
+    //             series: [{
+    //                 name: 'Reggane',
+    //                 data: [16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8, 38.4, 35.5, 29.2,
+    //                     22.0, 17.8]
+    //             }, {
+    //                 name: 'Tallinn',
+    //                 data: [-2.9, -3.6, -0.6, 4.8, 10.2, 14.5, 17.6, 16.5, 12.0, 6.5,
+    //                     2.0, -0.9]
+    //             }]
+    //         });
+    //     });
+    // });
+    Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        align: 'left',
+        text: 'Biểu đồ doanh thu cửa hàng'
+    },
+    accessibility: {
+        announceNewData: {
+            enabled: true
+        }
+    },
+    xAxis: {
+        type: 'category'
+    },
+    yAxis: {
+        title: {
+            text: 'Doanh thu'
+        }
 
-            // // dataChart.xAxis.labels.formatter = function() {
-            // //   var value = xAxis[this.value];
-            // //   return value !== 'undefined' ? value : this.value;
-            // // }
-            // dataChart.yAxis.labels.formatter = function() {
-            // var value = yAxis[this.value];
-            // return value !== 'undefined' ? value : this.value;
-            // }
-            // dataChart.tooltip.formatter = function() {
-            // return  this.x;
-            // }
-            // new Highcharts.Chart(dataChart);
-            Highcharts.chart('container', {
-                chart: {
-                    type: 'line'
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{point.y:.1f}%'
+            }
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    },
+
+    series: [
+        {
+            name: 'Browsers',
+            colorByPoint: true,
+            data: [
+                {
+                    name: 'Chrome',
+                    y: 63.06,
+                    drilldown: 'Chrome'
                 },
-                title: {
-                    text: 'Monthly Average Temperature'
+                {
+                    name: 'Safari',
+                    y: 19.84,
+                    drilldown: 'Safari'
                 },
-                subtitle: {
-                    text: 'Source: ' +
-                        '<a href="https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature" ' +
-                        'target="_blank">Wikipedia.com</a>'
+                {
+                    name: 'Firefox',
+                    y: 4.18,
+                    drilldown: 'Firefox'
                 },
-                xAxis: {
-                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                {
+                    name: 'Edge',
+                    y: 4.12,
+                    drilldown: 'Edge'
                 },
-                yAxis: {
-                    title: {
-                        text: 'Temperature (°C)'
-                    }
+                {
+                    name: 'Opera',
+                    y: 2.33,
+                    drilldown: 'Opera'
                 },
-                plotOptions: {
-                    line: {
-                        dataLabels: {
-                            enabled: true
-                        },
-                        enableMouseTracking: false
-                    }
+                {
+                    name: 'Internet Explorer',
+                    y: 0.45,
+                    drilldown: 'Internet Explorer'
                 },
-                series: [{
-                    name: 'Reggane',
-                    data: [16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8, 38.4, 35.5, 29.2,
-                        22.0, 17.8]
-                }, {
-                    name: 'Tallinn',
-                    data: [-2.9, -3.6, -0.6, 4.8, 10.2, 14.5, 17.6, 16.5, 12.0, 6.5,
-                        2.0, -0.9]
-                }]
-            });
-        });
-    });
+                {
+                    name: 'Other',
+                    y: 1.582,
+                    drilldown: null
+                }
+            ]
+        }
+    ],
+    drilldown: {
+        breadcrumbs: {
+            position: {
+                align: 'right'
+            }
+        },
+        series: [
+            {
+                name: 'Chrome',
+                id: 'Chrome',
+                data: [
+                    [
+                        'v65.0',
+                        0.1
+                    ],
+                    [
+                        'v64.0',
+                        1.3
+                    ],
+                    [
+                        'v63.0',
+                        53.02
+                    ],
+                    [
+                        'v62.0',
+                        1.4
+                    ],
+                    [
+                        'v61.0',
+                        0.88
+                    ],
+                    [
+                        'v60.0',
+                        0.56
+                    ],
+                    [
+                        'v59.0',
+                        0.45
+                    ],
+                    [
+                        'v58.0',
+                        0.49
+                    ],
+                    [
+                        'v57.0',
+                        0.32
+                    ],
+                    [
+                        'v56.0',
+                        0.29
+                    ],
+                    [
+                        'v55.0',
+                        0.79
+                    ],
+                    [
+                        'v54.0',
+                        0.18
+                    ],
+                    [
+                        'v51.0',
+                        0.13
+                    ],
+                    [
+                        'v49.0',
+                        2.16
+                    ],
+                    [
+                        'v48.0',
+                        0.13
+                    ],
+                    [
+                        'v47.0',
+                        0.11
+                    ],
+                    [
+                        'v43.0',
+                        0.17
+                    ],
+                    [
+                        'v29.0',
+                        0.26
+                    ]
+                ]
+            },
+            {
+                name: 'Firefox',
+                id: 'Firefox',
+                data: [
+                    [
+                        'v58.0',
+                        1.02
+                    ],
+                    [
+                        'v57.0',
+                        7.36
+                    ],
+                    [
+                        'v56.0',
+                        0.35
+                    ],
+                    [
+                        'v55.0',
+                        0.11
+                    ],
+                    [
+                        'v54.0',
+                        0.1
+                    ],
+                    [
+                        'v52.0',
+                        0.95
+                    ],
+                    [
+                        'v51.0',
+                        0.15
+                    ],
+                    [
+                        'v50.0',
+                        0.1
+                    ],
+                    [
+                        'v48.0',
+                        0.31
+                    ],
+                    [
+                        'v47.0',
+                        0.12
+                    ]
+                ]
+            },
+            {
+                name: 'Internet Explorer',
+                id: 'Internet Explorer',
+                data: [
+                    [
+                        'v11.0',
+                        6.2
+                    ],
+                    [
+                        'v10.0',
+                        0.29
+                    ],
+                    [
+                        'v9.0',
+                        0.27
+                    ],
+                    [
+                        'v8.0',
+                        0.47
+                    ]
+                ]
+            },
+            {
+                name: 'Safari',
+                id: 'Safari',
+                data: [
+                    [
+                        'v11.0',
+                        3.39
+                    ],
+                    [
+                        'v10.1',
+                        0.96
+                    ],
+                    [
+                        'v10.0',
+                        0.36
+                    ],
+                    [
+                        'v9.1',
+                        0.54
+                    ],
+                    [
+                        'v9.0',
+                        0.13
+                    ],
+                    [
+                        'v5.1',
+                        0.2
+                    ]
+                ]
+            },
+            {
+                name: 'Edge',
+                id: 'Edge',
+                data: [
+                    [
+                        'v16',
+                        2.6
+                    ],
+                    [
+                        'v15',
+                        0.92
+                    ],
+                    [
+                        'v14',
+                        0.4
+                    ],
+                    [
+                        'v13',
+                        0.1
+                    ]
+                ]
+            },
+            {
+                name: 'Opera',
+                id: 'Opera',
+                data: [
+                    [
+                        'v50.0',
+                        0.96
+                    ],
+                    [
+                        'v49.0',
+                        0.82
+                    ],
+                    [
+                        'v12.1',
+                        0.14
+                    ]
+                ]
+            }
+        ]
+    }
+});
 </script>
--- resources/views/layouts/admin-layout.blade.php
+++ resources/views/layouts/admin-layout.blade.php
@@ -38,7 +38,8 @@
 
     <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/series-label.js"></script>
+    <script src="https://code.highcharts.com/modules/data.js"></script>
+    <script src="https://code.highcharts.com/modules/drilldown.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
     <script src="https://code.highcharts.com/modules/accessibility.js"></script>
